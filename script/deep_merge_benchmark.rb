a3ffbbb34bd7c7042491b0765a0e2ca9fb2405e7
Masahiro <watanabe@cadenza-tech.com>
2025-08-13 04:02:54 +0900

    Fix benchmark

--- script/deep_merge_benchmark.rb
+++ script/deep_merge_benchmark.rb
@@ -9,10 +9,10 @@ class DeepMergeBenchmark
   SPEED_RATIO_THRESHOLD = 0.1
 
   BENCHMARK_METHODS = {
-    'ActiveSupport' => :deep_merge,
-    'DeepMerge' => :dm_deep_merge,
-    'SinDeepMerge' => :sin_deep_merge,
-    'Scratch' => :scratch_deep_merge
+    'DeepMerge' => [:dm_deep_merge, :dm_deep_merge!],
+    'SinDeepMerge' => [:sin_deep_merge, :sin_deep_merge!],
+    'ActiveSupport' => [:deep_merge, :deep_merge!],
+    'Scratch' => [:scratch_deep_merge]
   }.freeze
 
   def self.run
@@ -49,12 +49,14 @@ class DeepMergeBenchmark
       x.warmup = 0.5
       x.quiet = true
 
-      BENCHMARK_METHODS.each do |lib_name, method|
-        x.report("#{lib_name} - deep_merge") do |times|
-          i = 0
-          while i < times
-            hash1.send(method, hash2)
-            i += 1
+      BENCHMARK_METHODS.each do |lib_name, methods|
+        methods.each do |method|
+          x.report("#{lib_name} - deep_merge") do |times|
+            i = 0
+            while i < times
+              hash1.send(method, hash2)
+              i += 1
+            end
           end
         end
       end
@@ -67,12 +69,14 @@ class DeepMergeBenchmark
       x.warmup = 0.5
       x.quiet = true
 
-      BENCHMARK_METHODS.each do |lib_name, method|
-        x.report("#{lib_name} - deep_merge (#{block_name})") do |times|
-          i = 0
-          while i < times
-            hash1.send(method, hash2, &block)
-            i += 1
+      BENCHMARK_METHODS.each do |lib_name, methods|
+        methods.each do |method|
+          x.report("#{lib_name} - deep_merge (#{block_name})") do |times|
+            i = 0
+            while i < times
+              hash1.send(method, hash2, &block)
+              i += 1
+            end
           end
         end
       end
@@ -92,7 +96,7 @@ class DeepMergeBenchmark
 
   def create_result_table(test_name, results)
     Terminal::Table.new(
-      title: "Benchmark Result: #{test_name}",
+      title: "Benchmark Result (#{test_name})",
       headings: ['Name', 'Iteration Per Second', 'Speed Ratio'],
       rows: format_result_rows(results)
     )
@@ -102,7 +106,7 @@ class DeepMergeBenchmark
     sorted_results = results.sort_by { |_key, value| value }.reverse
     fastest_speed = sorted_results.first[1]
     sorted_results.map do |key, value|
-      [key, format('%.1f', value), calculate_speed_ratio(fastest_speed, value)]
+      [key.sub(/(dm|sin|scratch)_/, ''), format('%.1f', value), calculate_speed_ratio(fastest_speed, value)]
     end
   end
 
